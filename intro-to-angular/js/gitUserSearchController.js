145b5cbc2175aa60a4b8a2f3e52eab1ab9a0033b
thisdotrob <thisdotrob@gmail.com>
2015-12-01 19:17:46 +0000

    Add search functionality

--- intro-to-angular/js/gitUserSearchController.js
+++ intro-to-angular/js/gitUserSearchController.js
@@ -1,3 +1,20 @@
 githubUserSearch.controller('GitUserSearchController', [function(){
 
+  var self = this;
+
+  self.doSearch = function() {
+    self.searchResult = {
+      "items": [
+        { "login": "thisdotrob",
+          "avatar_url": "https://avatars3.githubusercontent.com/u/12902589?v=3",
+          "html_url": "https://github.com/thisdotrob"
+        },
+        { "login": "jbhdeconinck",
+          "avatar_url": "https://avatars1.githubusercontent.com/u/14220697?v=3",
+          "html_url": "https://github.com/jbhdeconinck"
+        }
+      ]
+    };
+  };
+
 }]);
--- intro-to-angular/test/gitUserSearchController.spec.js
+++ intro-to-angular/test/gitUserSearchController.spec.js
@@ -3,13 +3,32 @@ describe('GitUserSearchController', function(){
 
   var ctrl;
 
-  beforeEach(inject(function($controller){
+  beforeEach(inject(function($controller) {
     ctrl = $controller('GitUserSearchController');
   }));
 
   it('initialises with an empty search result and term', function() {
     expect(ctrl.searchResult).toBeUndefined();
     expect(ctrl.searchTerm).toBeUndefined();
+  });
+
+  describe('when searching for a user', function(){
+
+    var items = [
+      { "login": "thisdotrob",
+        "avatar_url": "https://avatars3.githubusercontent.com/u/12902589?v=3",
+        "html_url": "https://github.com/thisdotrob"
+      },
+      { "login": "jbhdeconinck",
+        "avatar_url": "https://avatars1.githubusercontent.com/u/14220697?v=3",
+        "html_url": "https://github.com/jbhdeconinck"
+      }
+    ];
+
+    it('displays search results', function(){
+      ctrl.doSearch();
+      expect(ctrl.searchResult.items).toEqual(items);
+    });
   });
 
 });
