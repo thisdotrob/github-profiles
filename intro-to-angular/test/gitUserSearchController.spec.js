b906a8e97eb8a56ae783e1214162da3347d56b7a
thisdotrob <thisdotrob@gmail.com>
2015-12-02 17:13:53 +0000

    Refactor Search factory mock to allow different return values by test

--- intro-to-angular/test/gitUserSearchController.spec.js
+++ intro-to-angular/test/gitUserSearchController.spec.js
@@ -1,18 +1,19 @@
 describe('GitUserSearchController', function() {
 
   var ctrl;
+  var scope;
+  var searchSpy = jasmine.createSpyObj('searchSpy', ['query']);
 
-  var _Search_ = {
-    query: function(){
-      return { then: function(cb) { cb({data: {items: items}});}};
-    }
-  };
+  console.log(searchSpy);
 
   beforeEach(module('GitUserSearch'));
-  beforeEach(module({Search: _Search_}));
 
-  beforeEach(inject(function($controller){
-    ctrl = $controller('GitUserSearchController');
+  beforeEach(inject(function($rootScope, $controller, $q){
+    scope = $rootScope.$new();
+    ctrl = $controller('GitUserSearchController', {
+      $scope: scope,
+      Search: searchSpy
+    });
   }));
 
   it('initialises with an empty search result and term', function() {
@@ -25,7 +26,11 @@ describe('GitUserSearchController', function() {
 
     it('displays search results', function() {
       ctrl.searchTerm = 'hello';
+      inject(function($q) {
+        searchSpy.query.and.returnValue($q.when({data:{items: items}}));
+      });
       ctrl.doSearch();
+      scope.$apply();
       expect(ctrl.searchResult.items).toEqual(items);
     });
   });
