e137590d57e5d6d55dde8c4f71dad095dcc663ae
thisdotrob <thisdotrob@gmail.com>
2015-12-03 15:24:27 +0000

    Add feature tests

--- intro-to-angular/test/e2e/githubProfileFeature.js
+++ intro-to-angular/test/e2e/githubProfileFeature.js
@@ -1,6 +1,54 @@
 describe('Github profile finder', function() {
-  it('has a title', function() {
+
+  beforeEach(function() {
     browser.get('http://localhost:8080');
+  });
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('Github user search');
   });
+
+  it('display the search label', function() {
+    element(by.model('searchCtrl.searchTerm')).sendKeys('thisdotrob');
+    element(by.css('.btn')).click();
+    expect(element(by.binding('searchCtrl.searchTerm')).getText()).toEqual('thisdotrob');
+  });
+
+  it('displays the username in the search result', function() {
+    element(by.model('searchCtrl.searchTerm')).sendKeys('thisdotrob');
+    element(by.css('.btn')).click();
+    expect(element(by.binding('user.login')).getText()).toEqual('thisdotrob');
+  });
+
+  it('displays the username in the search result', function() {
+    element(by.model('searchCtrl.searchTerm')).sendKeys('thisdotrob');
+    element(by.css('.btn')).click();
+    var results = element.all(
+      by.repeater('user in searchCtrl.searchResult.items')
+    );
+    var username = results.first().element(by.binding('user.login'));
+    expect(username.getText()).toEqual('thisdotrob');
+  });
+
+  it('displays the user\'s profile picture in the search results', function() {
+    element(by.model('searchCtrl.searchTerm')).sendKeys('thisdotrob');
+    element(by.css('.btn')).click();
+    var results = element.all(
+      by.repeater('user in searchCtrl.searchResult.items')
+    );
+    var img = results.first().element(by.id('profilepicture'));
+    var src = 'https://avatars.githubusercontent.com/u/12902589?v=3&s=150';
+    expect(img.isPresent()).toBe(true);
+    expect(img.getAttribute('src')).toEqual(src);
+  });
+
+  it('displays multiple search results', function() {
+    element(by.model('searchCtrl.searchTerm')).sendKeys('thisdot');
+    element(by.css('.btn')).click();
+    var results = element.all(
+      by.repeater('user in searchCtrl.searchResult.items')
+    );
+    expect(results.count()).toEqual(8);
+  });
+
 });
